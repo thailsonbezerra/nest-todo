ead9edf315324ca9868091f584e513e189452de8
Thailson Bezerra <thailsonbezerra@outlook.com>
2023-03-29 21:35:16 -0300

    chore: configurado env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config/dist';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 7432,
-      username: 'postgres',
-      password: '1234',
-      database: 'postgres',
-      entities: [],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: Number(configService.get('DB_PORT', 7432)),
+        username: configService.get('DB_USERNAME', 'postgres'),
+        password: configService.get('DB_PASSWORD', '1234'),
+        database: configService.get('DB_DATABASE', 'postgres'),
+        entities: [],
+        synchronize: true,
+      }),
     }),
   ],
   controllers: [],
