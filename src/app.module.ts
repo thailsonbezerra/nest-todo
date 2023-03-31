27b2ef47b46c454079b5eb01792f7944e4fda00d
Thailson Bezerra <thailsonbezerra@outlook.com>
2023-03-30 22:38:00 -0300

    feat: adicionada a entity todo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { ConfigService } from '@nestjs/config/dist';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoModule } from './app/todo/todo.module';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -16,8 +17,9 @@ import { TodoModule } from './app/todo/todo.module';
         username: configService.get('DB_USERNAME', 'postgres'),
         password: configService.get('DB_PASSWORD', '1234'),
         database: configService.get('DB_DATABASE', 'postgres'),
-        entities: [],
+        entities: [join(__dirname, '/../**/**.entity{.ts,.js}')],
         synchronize: true,
+        autoLoadEntities: true,
       }),
     }),
     TodoModule,
