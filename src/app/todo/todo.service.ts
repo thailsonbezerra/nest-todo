0e35e3a2da9fffb58009d7f5634537c8a1703b3d
Thailson Bezerra <thailsonbezerra@outlook.com>
2023-04-02 15:03:24 -0300

    feat: adicionado os metodos em todo service

--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Todo } from './entity/todo.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectRepository(Todo)
+    private readonly todoRepository: Repository<Todo>,
+  ) {}
+
+  async findAll() {
+    return this.todoRepository.find();
+  }
+
+  async findOneOrFail(id) {
+    try {
+      return await this.todoRepository.findOneOrFail(id);
+    } catch (error) {
+      throw new NotFoundException(error.message);
+    }
+  }
+
+  async create(data) {
+    return await this.todoRepository.save(this.todoRepository.create(data));
+  }
+
+  async update(id, data) {
+    const todo = await this.findOneOrFail(id);
+
+    this.todoRepository.merge(todo, data);
+    return await this.todoRepository.save(todo);
+  }
+
+  async deleteById(id) {
+    await this.findOneOrFail(id);
+    await this.todoRepository.softDelete(id);
+  }
+}
